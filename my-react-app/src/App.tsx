de10e284ccc2bc73756644f6e29514b7e8079147
Marina <98844023+MarinaMassot@users.noreply.github.com>
2024-10-23 17:09:15 +0200

    Update App.tsx
    bug

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,22 +1,60 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard"
+import {useState} from "react";
+// import NavBar from "./components/NavBar";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 
 function App() {
+  // state pokemonIndex initialisé à 0
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+  // au clic +1 dans la limite de la longueur de pokemonList
+  const handleClickNext = () => {
+    setpokemonIndex((pokemonIndex) => 
+      pokemonIndex < pokemonList.length - 1 ? pokemonIndex + 1 : 0
+    );
+  };
+// au clic -1 dans la limite de la longueur de pokemonList
+  const handleClickBefore = () => {
+    setpokemonIndex((pokemonIndex) =>
+      pokemonIndex > 0 ? pokemonIndex - 1 : pokemonList.length - 1
+  );
+  } ;
+
   return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
+    <figure>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {/* boutons précédents et suivants */}
+    <button type="button" onClick={handleClickBefore}>Précédent</button>
+    <button type="button" onClick={handleClickNext}>Suivant</button>
+    </figure>
   );
 }
+
+
 export default App;
